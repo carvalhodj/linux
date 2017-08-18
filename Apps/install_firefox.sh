dfa22c9831ba073d71d3240e9a9648cb8213fe98
Daniel Carvalho <danieljosecarvalho@gmail.com>
2017-08-18 08:31:18 -0300

    Update instructions

--- Apps/install_firefox.sh
+++ Apps/install_firefox.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Install the latest Mozilla Firefox stable
+# Change the lang parameter at the end of the link to yours. Here the link to see your lang code: https://goo.gl/F9N7zU
 # based on https://linuxconfig.org/how-to-install-latest-firefox-browser-on-debian-9-stretch-linux
 
 wget -O FirefoxSetup.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=pt-BR"
